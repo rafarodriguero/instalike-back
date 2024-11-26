53ec641278a927ad0799aba9b7900c2b24f53168
rafarodriguero <rafaelrodriguero@msn.com>
2024-11-26 14:33:42 +0000

    Quarto commit da aplicação

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -8,6 +8,7 @@ export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
   // Envia os posts como resposta JSON com status 200 (OK)
   res.status(200).json(posts);
+  console.log("listagem de Posts");
 }
 
 // Função assíncrona para criar um novo post
@@ -21,7 +22,6 @@ export async function postarNovoPost(req, res) {
     res.status(200).json(postCriado);
   } catch (erro) {
     // Imprime o erro no console para depuração
-    console.error(erro.message);
     // Envia uma mensagem de erro como resposta JSON com status 500 (Erro interno do servidor)
     res.status(500).json({ "Erro": "Falha na requisição" });
   }
@@ -30,23 +30,29 @@ export async function postarNovoPost(req, res) {
 // Função assíncrona para fazer upload de uma imagem e criar um novo post
 export async function uploadImagem(req, res) {
   // Cria um objeto com os dados do novo post, incluindo o nome do arquivo da imagem
+  console.log("Inicio da função uploadImagem");
   const novoPost = {
     descricao: "",
     imgUrl: req.file.originalname,
     alt: ""
   };
+  console.log("")
   try {
     // Chama a função do modelo para criar um novo post no banco de dados
     const postCriado = await criarPost(novoPost);
     // Gera um novo nome para a imagem, incluindo o ID do post
     const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    console.log("variavel imagemAtualizada " + imagemAtualizada);
     // Renomeia o arquivo da imagem para o novo nome
     fs.renameSync(req.file.path, imagemAtualizada);
+    console.log("req.file.path " + req.file.path);
     // Envia o post criado como resposta JSON com status 200 (OK)
     res.status(200).json(postCriado);
+    console.log("Json PostCriado " + json(postCriado));
   } catch (erro) {
     // Imprime o erro no console para depuração
     console.error(erro.message);
+    console.log(erro.message);
     // Envia uma mensagem de erro como resposta JSON com status 500 (Erro interno do servidor)
     res.status(500).json({ "Erro": "Falha na requisição" });
   }
